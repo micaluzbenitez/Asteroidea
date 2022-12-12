c727132c38788d5dc679740dc0e89858516f2f4c
Guido Tello <lpmaleinn@gmail.com>
2022-12-12 20:12:09 -0300

    Created ButtonTouch Script
    Just to activate the sound on clic.
    //Button.onClick.AddListener(Function) throws context error.

---
+++ Asteroidea_2022/Assets/Scripts/UI/ButtonTouch.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ButtonTouch : MonoBehaviour
+{
+	//private Button thisButton;
+
+
+	//void Start()
+	//{
+	//	thisButton = GetComponent<Button>();
+	//}
+
+	public void PlaySound()
+    {
+        WwiseInterface.ExecuteWwiseEvent(WwiseInterface.WwiseEvents.Button_Touch, this.gameObject);
+    }
+}
