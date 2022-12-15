45c87d4a35b4049ffb59ee3769d988f7947efee2
shrodri <rodrigoabrahancerimeli@gmail.com>
2022-12-15 00:35:44 -0300

    wwise audio udpate
    modificacion duracion sonido medusa

--- Asteroidea_2022/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Asteroidea_2022/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -24,7 +24,7 @@ namespace AK
         static const AkUniqueID PLAY_FISH_BITE = 1866806893U;
         static const AkUniqueID PLAY_IMPACT_PLATAFORM = 3795799651U;
         static const AkUniqueID PLAY_JELLYFISH_IMPACT = 796436477U;
-        static const AkUniqueID PLAY_JUEGO_ESTRELLA_MUSICA_TEMA1_1 = 3635214037U;
+        static const AkUniqueID PLAY_MX = 2447410425U;
         static const AkUniqueID PLAY_PICKUP = 3860455926U;
         static const AkUniqueID STARFISH_MOVEMENTS = 1875812120U;
         static const AkUniqueID STOP_SFX_MUTEABLES = 767809362U;
@@ -76,6 +76,7 @@ namespace AK
 
     namespace GAME_PARAMETERS
     {
+        static const AkUniqueID METROS = 1603003027U;
         static const AkUniqueID MUSICAVOLUMEN = 3465895069U;
         static const AkUniqueID SFXVOLUME = 988953028U;
     } // namespace GAME_PARAMETERS
